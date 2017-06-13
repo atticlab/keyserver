9c27d055600e1778603783edabb32f13da8be0ae
Junt Van <juntvan@gmail.com>
2017-06-13 17:35:43 +0300

    cors fixed

--- src/php/app/api/controllers/ControllerBase.php
+++ src/php/app/api/controllers/ControllerBase.php
@@ -22,7 +22,7 @@ class ControllerBase extends \Phalcon\Mvc\Controller
 
                 if ($this->request->isOptions()) {
                     $this->response->setHeader('Access-Control-Allow-Headers',
-                        'Origin, X-CSRF-Token, X-Requested-With, X-HTTP-Method-Override, Content-Range, Content-Disposition, Content-Type, Authorization');
+                        'Nonce, Signature, Origin, X-CSRF-Token, X-Requested-With, X-HTTP-Method-Override, Content-Range, Content-Disposition, Content-Type, Authorization');
                     $this->response->setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, DELETE');
                     $this->response->sendHeaders();
                     exit;
